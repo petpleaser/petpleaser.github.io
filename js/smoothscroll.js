ebeb88696313bb9fe004bd02003f757d529e832c
CoopGroup <helloworldstudioelem+coopsmallwood@gmail.com>
2020-04-13 19:08:17 +0000

    More content

--- js/smoothscroll.js
+++ js/smoothscroll.js
@@ -31,3 +31,18 @@ a=b.extend({},c.defaults,a);return a.lazy?this.bind(a.event,function(d){var c=b(
 
 // Initialize all .smoothScroll links
 jQuery(function($){ $.localScroll({filter:'.smoothScroll'}); });
+$(document).on("scroll", function () {
+var pageTop = $(document).scrollTop()
+var pageBottom = pageTop + $(window).height()
+var tags = $("section")
+
+for (var i = 0; i < tags.length; i++) {
+var tag = tags[i]
+
+if ($(tag).position().top < pageBottom) {
+$(tag).addClass("visible")
+} else {
+  $(tag).removeClass("visible")
+}
+}
+})
